5a211ef73cfd16bf7f4469799ec1c74bdee18bf7
hmyo2853 <hmyo2853@gmail.com>
2025-08-14 04:22:12 +0900

    Feature : shared/ui에 highlight 컴포넌트 추가

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,21 +1,8 @@
-export { Button } from "./Button"
-export type { ButtonProps } from "./Button"
-export { Card, CardContent, CardHeader, CardTitle } from "./Card"
-export type { CardProps, CardContentProps, CardHeaderProps, CardTitleProps } from "./Card"
-export { Dialog, DialogContent, DialogHeader, DialogOverlay, DialogPortal, DialogTitle, DialogTrigger } from "./Dialog"
-export type { DialogContentProps, DialogHeaderProps, DialogTitleProps } from "./Dialog"
-export { Input } from "./Input"
-export type { InputProps } from "./Input"
-export { Select, SelectContent, SelectGroup, SelectItem, SelectValue, SelectTrigger } from "./Select"
-export type { SelectContentProps, SelectItemProps, SelectTriggerProps } from "./Select"
-export { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./Table"
-export type {
-  TableProps,
-  TableRowProps,
-  TableBodyProps,
-  TableCellProps,
-  TableHeadProps,
-  TableHeaderProps,
-} from "./Table"
-export { Textarea } from "./Textarea"
-export type { TextareaProps } from "./Textarea"
+export * from "./Button"
+export * from "./Card"
+export * from "./Dialog"
+export * from "./Input"
+export * from "./Select"
+export * from "./Table"
+export * from "./Textarea"
+export * from "./Highlights"
