fc179acd22b15377835ab3e8e24365eaa5559ae9
hmyo2853 <hmyo2853@gmail.com>
2025-08-15 09:52:08 +0900

    Feature : 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: "/front_6th_chapter2-3/",
+  build: {
+    rollupOptions: {
+      input: "./index.html",
+    },
+    outDir: "dist",
+    copyPublicDir: true,
+  },
   server: {
     proxy: {
       "/api": {
-        // target: 'https://jsonplaceholder.typicode.com',
         target: "https://dummyjson.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
